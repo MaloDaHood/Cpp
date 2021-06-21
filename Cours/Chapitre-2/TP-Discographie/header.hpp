839461590c5e4976ba24a76e6c6b990c36d42799
MaloDaHood <malomr2002@gmail.com>
2021-06-21 02:02:40 +0200

    parsing done

--- Cours/Chapitre-2/TP-Discographie/header.hpp
+++ Cours/Chapitre-2/TP-Discographie/header.hpp
@@ -7,7 +7,12 @@
 #include <limits>
 #include <algorithm>
 #include <unordered_set>
+#include <fstream>
 
 void Error();
+std::string SplitInput(std::string const &input, std::string &arguments);
+void Add(std::vector<std::string> const &infos, std::ofstream &tempFile);
+void ParseArguments(std::vector<std::string> &infos, std::string &arguments);
+void Save(std::string const &argument);
 
 #endif
--- Cours/Chapitre-2/TP-Discographie/main.cpp
+++ Cours/Chapitre-2/TP-Discographie/main.cpp
@@ -3,36 +3,37 @@
 int main()
 {
     system("cls");
-    std::string input;
-    while(input!="quit")
+    std::ofstream tempFile {"temp.csv"};
+    while(true)
     {
         std::cout << "> ";
+        std::string input;
         std::getline(std::cin, input);
-        std::transform(input.begin(), input.end(), input.begin(), [](unsigned char c){return std::tolower(c);});
-        std::string command {""};
-        for(char c : input)
-        {
-            if(isalpha(c))
-            {
-                command.push_back(c);
-            }
-            else if(isspace(c))
-            {
-                break;
-            }
-            else
-            {
-                Error();
-            }
-        }
+        std::string arguments {""};      
+        std::string command {SplitInput(input, arguments)};
+        std::string argumentsCopy {arguments};
+        std::transform(command.begin(), command.end(), command.begin(), [](unsigned char c){return std::tolower(c);});
         std::unordered_set<std::string> availableCommands {"add", "show", "save", "load", "quit"};
-        if(availableCommands.find(command)!=std::end(availableCommands))
-        {
-            
-        }
-        else
+        if(command=="quit")
+            return 0;
+        if(availableCommands.find(command)==std::end(availableCommands))
         {
             Error();
+            continue;
+        }
+        std::vector<std::string> infos; // infos[0]=song | infos[1]=album | infos[2]=artist
+        ParseArguments(infos, arguments);
+        if(command=="add")
+            Add(infos, tempFile);
+        else if(command=="save")
+        {
+            if(argumentsCopy.empty()) // need to chech for spaces
+            {
+                Error();
+                continue;
+            }
+            else
+                Save(argumentsCopy);
         }
     }
     return 0;
@@ -41,4 +42,70 @@ int main()
 void Error()
 {
     std::cout << "Error : Invalid Command." << std::endl;
+}
+
+std::string SplitInput(std::string const &input, std::string &arguments)
+{
+    std::string command {""};
+    for(int i {0}; i<input.length(); i++)
+    {
+        if(isalpha(input[i]))
+            command.push_back(input[i]);
+        else
+        {
+            for(int j {i+1}; j<input.length(); j++)
+                arguments.push_back(input[j]);
+            break;
+        }
+    }
+    return command;
+}
+
+void ParseArguments(std::vector<std::string> &infos, std::string &arguments)
+{
+    std::string delimiter {"|"};
+    arguments+=delimiter;
+    size_t pos {0};
+    while((pos=arguments.find(delimiter))!=std::string::npos)
+    {
+        infos.push_back(arguments.substr(0, pos));
+        arguments.erase(0, pos+delimiter.length());
+    }
+    if(infos.size()<3)
+    {
+        for(int i {0}; i<=3-infos.size(); i++)
+        {
+            infos.push_back("UNKNOWN");
+        }
+    }
+    while(std::any_of(std::begin(infos), std::end(infos), 
+        [](std::string const &str){return isspace(str.front())||isspace(str.back());}))
+    {
+        for(std::string &str : infos)
+        {
+            if(isspace(str.front()))
+                str.erase(str.begin());
+            if(isspace(str.back()))
+                str.pop_back();
+        }
+    }
+    while(std::any_of(std::begin(infos), std::end(infos), 
+        [](std::string const &str){return str.empty();}))
+    {
+        for(int i {0}; i<infos.size(); i++)
+        {
+            if(infos[i].empty())
+                infos[i]="UNKNOWN";
+        }
+    }
+}
+
+void Add(std::vector<std::string> const &infos, std::ofstream &tempFile)
+{
+    tempFile << infos[0] << ',' << infos[1] << ',' << infos[2] << "\n";
+}
+
+void Save(std::string const &argument)
+{
+    std::cout << "yes";
 }
