7bfbf9392aae2c19c0f72fba41601b45dc8e0047
MaloDaHood <malomr2002@gmail.com>
2021-11-23 22:43:28 +0100

    Optimized a lot

--- Personal-Projects/Asynconf_2021/Exercice_1/main.cpp
+++ Personal-Projects/Asynconf_2021/Exercice_1/main.cpp
@@ -1,41 +1,17 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
 #include "base64.h"
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-    std::string formuleEncode;
-    std::cout << "Formule encodée ?" << std::endl;
-    std::getline(std::cin, formuleEncode);
-    std::string formuleDecode64 {base64_decode(formuleEncode)};
-    std::unordered_map<char, int> alphabet_map;
-    for(int i {0}; i < 26; i++)
+    if(argc < 2)
     {
-        alphabet_map[char(i+97)] = i+1;
+        std::cout << '[' << argv[0] << "] - Error : Missing argument." << std::endl;
+        return EXIT_FAILURE;
     }
-    for(size_t i {0}; i < formuleDecode64.length(); i++)
-    //for(char const &lettre : formuleDecode64)
+    for(char const &character : base64_decode(static_cast<std::string>(argv[1])))
     {
-        if(!std::isalpha(formuleDecode64[i]))
-        {
-            std::cout << formuleDecode64[i];
-            continue;
-        }
-        else
-        {
-            int num;
-            if(std::isupper(formuleDecode64[i])) num = alphabet_map[std::tolower(formuleDecode64[i])]-2;
-            else num = alphabet_map[formuleDecode64[i]]-2;
-            for(auto const &pair : alphabet_map)
-            {
-                if(pair.second == num)
-                {
-                    if(std::isupper(formuleDecode64[i])) std::cout << char(std::toupper(pair.first));
-                    else std::cout << pair.first;
-                }
-            }
-        }
+        if(std::isalpha(character)) std::cout << char(character-2);
+        else std::cout << character;
     }
     return EXIT_SUCCESS;
 }
