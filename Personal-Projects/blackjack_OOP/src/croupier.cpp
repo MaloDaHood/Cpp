108ae8d396d46736a027848cc75f8ea93d22562a
MaloDaHood <malomr2002@gmail.com>
2022-01-02 17:16:40 +0100

    Deleted useless or bad code

--- Personal-Projects/blackjack_OOP/src/croupier.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * @file croupier.cpp
- * @author MaloDaHood (malomr2002@gmail.com)
- * @brief The source code for the croupier class.
- * @date 2021-11-17
- */
--- Personal-Projects/blackjack_OOP/src/game.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * @file game.cpp
- * @author MaloDaHood (malomr2002@gmail.com)
- * @brief The source code for the game class.
- * @date 2021-11-17
- */
